d242a8374f21cc5b1f90b6c516c53f1dbb4a9f83
KaseiHaku <34833442+KaseiHaku@users.noreply.github.com>
2019-01-17 10:17:22 +0800

    Update DownloadServlet.java

--- src/kasei/servlet/servlet/DownloadServlet.java
+++ src/kasei/servlet/servlet/DownloadServlet.java
@@ -34,19 +34,21 @@ public class DownloadServlet extends HttpServlet {
         this.doGet(request, response);
     }
     
-    
-    public static void downloadStream(HttpServletResponse response, String downloadFileName) throws IOException {
+    /** todo 设置文件下载时的 http response header 参数
+     * @param response http 返回对象
+     * @param downloadFileName 下载的文件名
+     * */
+    public static void setDownLoadResponseHeader(HttpServletResponse response, String downloadFileName) throws IOException {
         response.reset(); // Clears any data that exists in the buffer as well as the status code and headers.
         // response.resetBuffer(); // Clears the content of the underlying buffer in the response without clearing headers or status code.
         downloadFileName = URLEncoder.encode(downloadFileName, "utf-8");
         response.setContentType("application/octet-stream"); // 表示时 二进制流 不知道下载的文件类型
         response.addHeader("Content-Disposition","attachment;filename=" + downloadFileName); // 表示时附件，需要下载
-        ServletOutputStream os = response.getOutputStream(); // 获取输出流
+        // ServletOutputStream sos = response.getOutputStream(); // 获取输出流
         /*
         PrintWriter w = response.getWriter();// 这个是获取字符输出流
         w.println("one row");
         */
-        os.flush(); // 强制将所有缓存的内容写入到流中
     }
     
 }
